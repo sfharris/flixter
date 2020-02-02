581dfe9abab672a7f0589a2687acd490f16c4208
sfharris <samharris5451@gmail.com>
2020-02-02 04:26:35 +0000

    lesson 31

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,33 +1,58 @@
 class Instructor::SectionsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:create]
-  before_action :require_authorized_for_current_section, only: [:update]
-  
-  def new
-    @course = Course.find(params[:course_id])
-    @section = Section.new
+    before_action :authenticate_user!
+    before_action :require_authorized_for_current_course, only: [:create]
+    before_action :require_authorized_for_current_section, only: [:new, :create]
+
+    def new
+        @section = Section.find(params[:section_id])
+        if @section.course.user != current_user
+        return render plain: 'Unauthorized', status: :unauthorized
+        end
+        @section = Section.new
+      end
+
+
+
+   def create
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
-  def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
   end
 
   private
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+      render plain: 'Unauthorized', status: :Unauthorized
     end
   end
 
   helper_method :current_course
+
   def current_course
+    if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def section_params
-    params.require(:section).permit(:title, :image)
+    params.require(:section).permit(:title, :row_order_position)
   end
+  
 end
